0a99bbdc1da36db22d4beb400d0843ed0a84f25e
ios8106 <ios8106@users-macintosh.local>
2019-03-26 21:26:02 -0300

    16.11 flotando contatos no mapa

--- Contato.m
+++ Contato.m
@@ -7,9 +7,12 @@
 //
 
 #import "Contato.h"
-
+#import <MapKit/MkAnnotation.h>
 @implementation Contato
 -(NSString *)description {
     return [NSString stringWithFormat:@"Nome: %@, Telefone: %@, Endereço: %@, Site: %@", self.nome, self.telefone, self.endereco, self.site];
+}
+-(CLLocationCoordinate2D)coordinate{
+    return CLLocationCoordinate2DMake([self.latitude doubleValue], [self.longitude doubleValue]);
 }
 @end
--- ContatosNoMapaViewController.swift
+++ ContatosNoMapaViewController.swift
@@ -12,6 +12,9 @@ import MapKit
 class ContatosNoMapaViewController: UIViewController {
 
     @IBOutlet weak var mapa: MKMapView!
+    var contatos: [Contato] = Array()
+    let dao:ContatoDao = ContatoDao.sharedInstance()
+    
     let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
@@ -20,6 +23,16 @@ class ContatosNoMapaViewController: UIViewController {
         let botaoLocalizacao = MKUserTrackingBarButtonItem(mapView: self.mapa)
         self.navigationItem.rightBarButtonItem = botaoLocalizacao
         // Do any additional setup after loading the view.
+        
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        self.contatos = dao.listaTodos()
+        self.mapa.addAnnotations(self.contatos)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        self.mapa.removeAnnotations(self.contatos)
     }
 
     override func didReceiveMemoryWarning() {
